5ae3bec32b3ea592ed2e6077e222558282595ec2
CeolaEastwood <48657175+CeolaEastwood@users.noreply.github.com>
2021-07-01 23:23:17 +0000

    Laid out future functions and variables.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+//~NOTES~
+/* LET vs VAR
+   function modules??
+   Helper function?? 
+   need event listeners 
+   need data validation */
 
 //variables that might need to be global
 //inflation
@@ -16,6 +21,7 @@ var annualPayment = 5000;
 var currentValue = 30000;
 
 //Rates for calculations
+var rateOfReturn = .07;
 var fakeRateOfReturn = .07;
 var inflation = .00225;//last 10 years only
 //CPI = avg 2.4
@@ -28,17 +34,21 @@ var housePaymentRate = .28;
 
 
 
+//possible functions
+
+/*function calc(){}
+
+function displayTaxComp(){}
+function displayRetireBud(){}
+function calcRetireBud(){}
 
 
+function calcMargTax (){}
 function findTaxBracket(){}
-
 function findRetireTaxBracket(){}
 
 function calcPortWeight (){}
-
-function portWeight (){}
-
-function calcRateOfReturn (){}
+function calcRateOfReturn (){} */
 
 function calcFutureValue (){
   var futureValue = currentValue * rateOfReturn + currentValue + annualPayment;
@@ -51,8 +61,3 @@ function calcFutureValue (){
 }
 
 calcFutureValue ();
-
-
-
-
-
